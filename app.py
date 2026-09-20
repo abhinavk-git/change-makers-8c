baf63e830d78c279ab1d9241dbe2d745af91aaff
abhinavk-git <abhinavkchessgrandmaster@gmail.com>
2026-09-20 14:44:50 +0530

    Increase padding spacer to push profile to the absolute bottom on tall screens

--- app.py
+++ app.py
@@ -167,7 +167,7 @@ with st.sidebar:
     if st.button("Add a Model", use_container_width=True):
         st.session_state.page = "Add a Model"
     
-    st.markdown("<br>" * 15, unsafe_allow_html=True) # Push to bottom
+    st.markdown("<br>" * 40, unsafe_allow_html=True) # Push to bottom
     
     # Profile Indicator with Circle
     first_letter = st.session_state.username[0].upper() if st.session_state.username else "?"
