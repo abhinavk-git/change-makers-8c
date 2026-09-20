82ddc376ec9166797e61d1791b445dd91c81ea4d
abhinavk-git <abhinavkchessgrandmaster@gmail.com>
2026-09-20 19:39:10 +0530

    Pin notification bell popover to the absolute top right corner using CSS

--- app.py
+++ app.py
@@ -44,6 +44,13 @@ hide_st_style = """
             div.stButton > button {
                 border-radius: 0px !important;
             }
+            /* Pin the notification bell to the absolute top right */
+            div[data-testid="stPopover"] {
+                position: fixed !important;
+                top: 15px !important;
+                right: 25px !important;
+                z-index: 999999 !important;
+            }
             </style>
             """
 st.markdown(hide_st_style, unsafe_allow_html=True)
